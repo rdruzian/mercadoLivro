838c7ac69a5820d1240d50bb7d6611dc2981ea32
Renato Druzian <ex161676@g.unicamp.br>
2023-06-17 23:44:24 -0300

    save

--- src/main/kotlin/br/com/mercadolivro/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/br/com/mercadolivro/mercadolivro/controller/CustomerController.kt
@@ -1,8 +1,13 @@
 package br.com.mercadolivro.mercadolivro.controller
 
+import br.com.mercadolivro.mercadolivro.controller.request.PostCustomerRequest
 import br.com.mercadolivro.mercadolivro.model.Customer
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -12,5 +17,11 @@ class CustomerController {
     @GetMapping
     fun getCustomer(): Customer{
         return Customer("1", "teste", "abc@email.com")
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun create(@RequestBody customer: PostCustomerRequest) {
+        println(customer)
     }
 }
---
+++ src/main/kotlin/br/com/mercadolivro/mercadolivro/controller/request/PostCustomerRequest.kt
@@ -0,0 +1,6 @@
+package br.com.mercadolivro.mercadolivro.controller.request
+
+data class PostCustomerRequest (
+        var name: String,
+        var email: String
+)
